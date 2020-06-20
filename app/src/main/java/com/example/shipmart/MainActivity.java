b12f475f90a3a6e90d4e1503ec8048015337841b
Sparsh <sparsh.script@gmail.com>
2020-06-20 18:11:55 +0530

    Layout correction

--- app/src/main/java/com/example/shipmart/MainActivity.java
+++ app/src/main/java/com/example/shipmart/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.shipmart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etSalary, etDairy, etMeat, etFruits, etStreetFood, etCafe, etPub, etRestaurant, etClothes,
+    etFootwear, etDaal, etBakery, etBeverages, etSnacks, etBeauty, etCleaning, etKitchen, etBabyCare,
+    etTransIn, etTransOut, etOthers;
+
+    TextView tvSalary, tvSaving;
+
+    Button btnSubmitExp, btnGo, btnPrint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etSalary = findViewById(R.id.etSalary);
+        etDairy = findViewById(R.id.etDa);
     }
 }
