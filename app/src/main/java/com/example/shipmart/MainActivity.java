328c0db3ae9dfbb501bd1b8e71c9fe27a414d889
Sparsh <sparsh.script@gmail.com>
2020-06-20 18:20:53 +0530

    Initialization of views

--- app/src/main/java/com/example/shipmart/MainActivity.java
+++ app/src/main/java/com/example/shipmart/MainActivity.java
@@ -21,6 +21,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         etSalary = findViewById(R.id.etSalary);
-        etDairy = findViewById(R.id.etDa);
+        etDairy = findViewById(R.id.etDairyAmt);
+        etMeat = findViewById(R.id.etMeatAmt);
+        etFruits = findViewById(R.id.etFruitsAmt);
+        etStreetFood = findViewById(R.id.etStreetFoodAmt);
+        etCafe = findViewById(R.id.etCafeAmt);
+        etPub = findViewById(R.id.etPubAmt);
+        etRestaurant = findViewById(R.id.etRestaurantAmt);
+        etClothes = findViewById(R.id.etClothesAmt);
+        etFootwear = findViewById(R.id.etFootwearAmt);
+        etDaal = findViewById(R.id.etDaalAmt);
+        etBakery = findViewById(R.id.etBakeryAmt);
+        etBeverages = findViewById(R.id.etBeveragesAmt);
+        etSnacks = findViewById(R.id.etSnacksAmt);
+        etBeauty = findViewById(R.id.etBeautyAmt);
+        etCleaning = findViewById(R.id.etCleaningAmt);
+        etKitchen = findViewById(R.id.etKitchenAmt);
+        etBabyCare = findViewById(R.id.etBabyCareAmt);
+        etTransIn= findViewById(R.id.etTransInAmt);
+        etTransOut = findViewById(R.id.etTransOutAmt);
+        etOthers = findViewById(R.id.etOthersAmt);
+        btnGo=findViewById(R.id.btnGo);
+        btnSubmitExp=findViewById(R.id.btnSubMitExp);
+        btnPrint=findViewById(R.id.btnPrint);
+        tvSalary=findViewById(R.id.tvSalary);
+        tvSaving=findViewById(R.id.tvSaving);
+
+
+
+
+
+
+
+
     }
 }
